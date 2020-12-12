2c309b98ef7c8be5dc21261f67c73872ca665d6d
Pragati215 <dhakanepragati4@gmail.com>
2020-12-12 21:35:23 +0530

    Refactor : Undefined issue is resolved from linklistOperation

--- Linklist/LinklistOperation.js
+++ Linklist/LinklistOperation.js
@@ -1,47 +1,46 @@
 var readlineSync = require("readline-sync");
 let list = require("./LinklistMain");
 
-function linklistOperations(userInput) {
+function linklistOperations() {
 
-    switch (userInput) {
-        case 1:
-            var lengthOflist = readlineSync.questionInt("enter the size of list: ");
+    for (i = 0; i < 10; i++) {
 
-            while (list.sizeOfList() < lengthOflist) {
-                let element = readlineSync.question("Enter number to add-");
-                list.add(parseInt(element));
-                this.size++;
-            }
-            break;
-        case 2:
-            console.log("Search the number from Linklist-");
-            let element = readlineSync.question("enter index to search -")
-            readlineSync.question(list.search(element));
-            break;
-        case 3:
-            console.log("Delete element from linklist");
-            let indexOfNum = readlineSync.question("enter index to delete no.-");
-            console.log(list.removeFrom(indexOfNum));
-            break;
+        let userInput = readlineSync.questionInt("enter choice 1.add 2.search 3.Delete 4.display 5.exit :");
+        if (userInput == 5) return;
 
-        case 4:
-            console.log("Display Linklist");
-            readlineSync.question(list.display());
-            break;
-        case 5:
-            console.log("Exit");
-            break;
-        default:
-            console.log("wrong choice ");
-            break;
+        switch (userInput) {
+            case 1:
+                var lengthOflist = readlineSync.questionInt("enter the size of list: ");
+
+                while ( list.sizeOfList() < lengthOflist ) {
+                    let element = readlineSync.question("Enter number to add-");
+                    list.add(parseInt(element));
+                    this.size++;
+                }
+                break;
+            case 2:
+                console.log("Search the number from Linklist-");
+                let element = readlineSync.question("enter index to search -")
+                readlineSync.question(list.search(element));
+                break;
+            case 3:
+                console.log("Delete element from linklist");
+                let indexOfNum = readlineSync.question("enter index to delete no.-");
+                console.log(list.removeFrom(indexOfNum));
+                break;
+
+            case 4:
+                console.log("Display Linklist");
+                readlineSync.question(list.display());
+                break;
+            case 5:
+                console.log("Exit");
+                break;
+            default:
+                console.log("wrong choice ");
+                break;
+        }
     }
 }
 
-for (i = 0; i < 9; i++) {
-
-    let userInput = readlineSync.question("enter choice 1.add 2.search 3.Delete 4.display 5.exit :");
-    if (userInput == 5) {
-        return;
-    }
-    console.log(linklistOperations(parseInt(userInput)));
-}
+linklistOperations();
